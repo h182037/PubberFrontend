d5bdbb21104caeb231ada9c32903b6e06fcc6a4c
Eivind Skrede <eivindls@hotmail.com>
2020-04-03 16:13:22 +0200

    test

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,13 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Calculator = () => {
+
+  const getDataFromBackendTest = () => {
+    fetch('https://swapi.co/api/people/1')
+    .then((response) => { response.json() })
+    .then((data) => {
+      console.log(data);
+    });
+  }
+
   return (
     <div>
-        <li>
-            <Link to='/'>Back to front page</Link>
-        </li>
-        <h1>Calculator</h1>
-        <p>Here you will find an advanced calculator to calculate your night out!</p>
+      <li>
+          <Link to='/'>Back to front page</Link>
+      </li>
+      <h1>Calculator</h1>
+      <p>Here you will find an advanced calculator to calculate your night out!</p>
+      <button onClick={getDataFromBackendTest}>
+        Test
+      </button>
     </div>
   );
 }
